1b7d2f3a78422fc5646ccd5835d56f7ff99da7d0
Somyah <92253618+SomyahAA@users.noreply.github.com>
2021-12-29 14:36:10 +0300

    good luck

--- app/src/main/java/com/example/tryvideocall/MainActivity.kt
+++ app/src/main/java/com/example/tryvideocall/MainActivity.kt
@@ -12,7 +12,8 @@ import android.content.pm.PackageManager
 import android.util.Log
 import android.view.SurfaceView
 import android.widget.FrameLayout
-
+import com.example.tryvideocall.agora.media.RtcTokenBuilder
+import io.agora.agorauikit.manager.AgoraRTC
 
 
 private const val PERMISSION_REQ_ID_RECORD_AUDIO = 22
@@ -23,10 +24,13 @@ class MainActivity : AppCompatActivity() {
     // Fill the App ID of your project generated on Agora Console.
     private val APP_ID = "abcbccc386c6473c9455f897b2bd2555"
     // Fill the channel name.
-    private val CHANNEL = "somyahChannel"
+    private val CHANNEL = "a"
     // Fill the temp token generated on Agora Console.
-    private val TOKEN = "006abcbccc386c6473c9455f897b2bd2555IACU1+srnKc9K7hGmjvnDMP4migJhWDe+y4BZHJHGFVPFJCWrnsAAAAAEACVNqlcyuLKYQEAAQDK4sph"
 
+
+
+    //private val TOKEN = "006abcbccc386c6473c9455f897b2bd2555IAB2hHnD0+QHANO7ARbdRFcmcs4ghbI06a7YtsgfvIlQvkO+t+gAAAAAEACu7KwLRYfNYQEAAQBFh81h"
+    private val TOKEN = "006abcbccc386c6473c9455f897b2bd2555IABnnH3ooSuEzOhCaIgBkREhD6R9t533K+Xs+D8qG2uPJEO+t+hPAoleIgB3oBqax5DNYQQAAQBXTcxhAgBXTcxhAwBXTcxhBABXTcxh"
     private var mRtcEngine: RtcEngine ?= null
 
     private val mRtcEventHandler = object : IRtcEngineEventHandler() {
@@ -59,10 +63,42 @@ class MainActivity : AppCompatActivity() {
             initializeAndJoinChannel()
         }
     }
+
+
+    private fun startVideoCall( ): String {
+
+
+
+        val   appCertificate = "b6ae56a99aa440f3a71291f046254aca";
+
+
+        val  uid = 2082341273;
+        val  expirationTimeInSeconds = 3600;
+
+        val token : RtcTokenBuilder =  RtcTokenBuilder()
+        val  timestamp :Int = (System.currentTimeMillis() / 1000 + expirationTimeInSeconds).toInt()
+
+
+        val result: String = token.buildTokenWithUid(APP_ID, appCertificate,
+            CHANNEL, uid, RtcTokenBuilder.Role.Role_Publisher, timestamp);
+
+        Log.d("TAG", "startVideoCall999: $result")
+
+        return result
+    }
+
+
     private fun initializeAndJoinChannel() {
         try {
+
+
             mRtcEngine = RtcEngine.create(baseContext, APP_ID, mRtcEventHandler)
+
+           
+
+            Log.d("TAG", "initializeAndJoinChannel: ")
         } catch (e: Exception) {
+            Log.e("TAG", "initializeAndJoinChannel: ", e)
         }
         // By default, video is disabled, and you need to call enableVideo to start a video stream.
         mRtcEngine!!.enableVideo()
@@ -73,9 +109,10 @@ class MainActivity : AppCompatActivity() {
         localContainer.addView(localFrame)
         // Pass the SurfaceView object to Agora so that it renders the local video.
         mRtcEngine!!.setupLocalVideo(VideoCanvas(localFrame, VideoCanvas.RENDER_MODE_FIT, 0))
-
+        startVideoCall()
         // Join the channel with a token.
-        mRtcEngine!!.joinChannel(TOKEN, CHANNEL, "", 0)
+        //mRtcEngine.
+        mRtcEngine!!.joinChannel(startVideoCall().replace('/','+'), CHANNEL, "", 0)
     }
 
     // Kotlin
@@ -91,7 +128,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-
         mRtcEngine?.leaveChannel()
         RtcEngine.destroy()
     }
